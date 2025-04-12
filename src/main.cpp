2d02039d41a9112f39eb114ed83588532e629678
Perry Miller <perry@modelspace.com>
2025-04-11 20:12:32 -0400

    This commit is about 3 hours and 10 minutes of work

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include <vector>
 
 
@@ -20,17 +23,32 @@ typedef std::vector < double > Heights;
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+//	Constants used below.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+const double HORIZONTAL_RESOLUTION = 30;
+const double VERTICAL_RESOLUTION = 11;
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
 //	Safely return an unsigned int.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-unsigned int getUint ( const char *str )
+inline unsigned int getUint ( const char *str )
 {
+	if ( !str )
+	{
+		throw std::runtime_error ( "Invalid string when converting to unsigned int" );
+	}
+
 	int answer = std::atoi ( str );
 
 	if ( answer < 0 )
 	{
-		throw std::runtime_error ( "Invalid unsigned int" );
+		throw std::runtime_error ( "String is invalid unsigned int" );
 	}
 
 	return static_cast < unsigned int > ( answer );
@@ -43,11 +61,86 @@ unsigned int getUint ( const char *str )
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-Heights getHeights ( const  std::ifstream &in )
+inline Heights getHeights ( unsigned int width, unsigned int height, std::ifstream &in )
 {
+	// Check input.
+	if ( ( width <= 0 ) || ( height <= 0 ) )
+	{
+		throw std::invalid_argument ( "Width and height must be greater than zero" );
+	}
+
+	// Make the container of heights and size it correctly.
+	typedef std::vector < std::uint8_t > RawHeights;
+	RawHeights rh;;
+	rh.resize ( width * height );
+
+	// The size of all the data in bytes.
+	const std::size_t dataSize = rh.size() * ( sizeof ( RawHeights::size_type ) );
+
+	// Read all the values into the container.
+	in.read ( reinterpret_cast < char * > ( &rh[0] ), dataSize );
+
+	// Make sure it all read correctly.
+	if ( ( static_cast < std::streamsize > ( dataSize ) ) != in.gcount() )
+	{
+		std::ostringstream out;
+		out << "Read " << in.gcount() << " bytes but expected " << dataSize;
+		throw std::runtime_error ( out.str() );
+	}
+
+	// Convert to the correct container type.
 	Heights heights;
-	return heights; // TODO
+	heights.resize ( width * height );
+	for ( std::size_t i = 0; i < rh.size(); ++i )
+	{
+		heights[i] = static_cast < double > ( rh[i] * ELEVATION_RESOLUTION );
+	}
+
+	// Return the heights. This should be a move-copy so not expensive.
+	return heights;
 }
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//	Run the program.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void run ( int argc, char **argv )
+{
+	// Get the width and height.
+	const unsigned int width  = getUint ( argv[1] );
+	const unsigned int height = getUint ( argv[2] );
+
+	// Check the width and height.
+	if ( ( width <= 0 ) || ( height <= 0 ) )
+	{
+		throw std::invalid_argument ( "Width and height must be greater than zero" );
+	}
+
+	// Open the input file in binary.
+	std::ifstream in ( argv[1], std::ios::binary );
+
+	// Did it open?
+	if ( !in.is_open() )
+	{
+		std::ostringstream out;
+		out << "Could not open input file: " << argv[1];
+		throw std::runtime_error ( out.str() );
+	}
+
+	// Read the file into a vector of heights.
+	const Heights heights = getHeights ( width, height, in );
+
+	// Get the distance along the path.
+	const double dist = getDistance ( width, height, heights );
+
+
+	std::cout << "Distance along the path is " << dist << " meters" << std::endl;
+	return 0;
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -63,34 +156,27 @@ int main ( int argc, char **argv )
 		std::cerr << "Usage: " << argv[0] << " <width> <height> <input file>" << std::endl;
 		return 1;
 	}
-	// Get the width and height.
-	const unsigned int width  = getUint ( argv[1] );
-	const unsigned int height = getUint ( argv[2] );
 
-	// Check the width and height.
-	if ( ( width <= 0 ) || ( height <= 0 ) )
+	// Safely run the program.
+	try
 	{
-		std::cerr << "Width and height must be greater than zero" << std::endl;
+		run ( argc, argv );
+	}
+
+	// Catch standard exceptions.
+	catch ( const std::exception &e )
+	{
+		std::cerr << "Exception caught: " << e.what() << std::endl;
 		return 1;
 	}
 
-	// Open the input file in binary.
-	std::ifstream in ( argv[1], std::ios::binary );
-
-	// Did it open?
-	if ( !in.is_open() )
+	// Catch all other exceptions.
+	catch ( ... )
 	{
-		std::cerr << "Could not open input file: " << argv[1] << std::endl;
+		std::cerr << "Unknown exception caught" << std::endl;
 		return 1;
 	}
 
-	// Read the file into a vector of heights.
-	const Heights heights = getHeights ( width, height, in );
-
-	// Get the distance along the path.
-	const double dist = getDistance ( width, height, heights );
-
-
-	std::cout << "Distance along the path is " << dist << " meters" << std::endl;
+	// If we get to here then it worked.
 	return 0;
 }
